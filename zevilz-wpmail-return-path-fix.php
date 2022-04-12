b43fadf47decf92f89f38a23b4ee3cb0bdcd9d41
Alexandr <zevilz13@yandex.ru>
2022-04-13 03:01:04 +0500

    Update zevilz-wpmail-return-path-fix.php

--- zevilz-wpmail-return-path-fix.php
+++ zevilz-wpmail-return-path-fix.php
@@ -4,9 +4,10 @@ Plugin Name: WP mail Return-Path fix
 Description: Adds sender email address to "Return-Path" header
 Version: 1.0
 Author: zEvilz
-Author URI: https://github.com/zevilz/zevilz-wpmail-return-path-fix
+Author URI: https://zevilz.dev
+Plugin URI: https://github.com/zevilz/zevilz-wpmail-return-path-fix
 License: MIT
-*/
+ */
 
 add_action( 'phpmailer_init', function( $phpmailer ) {
 	$phpmailer->Sender = $phpmailer->From;
